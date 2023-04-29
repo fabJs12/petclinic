a623ff4ad63f4a1ed0855cd2b9dca4c8626d36a8
Jaime Gomez <jgomez@Jaimes-MacBook-Pro.local>
2023-04-29 14:18:25 -0500

    Update

--- src/test/java/com/tecsup/petclinic/services/OwnerServiceTest.java
+++ src/test/java/com/tecsup/petclinic/services/OwnerServiceTest.java
@@ -1,8 +1,6 @@
 package com.tecsup.petclinic.services;
 
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
